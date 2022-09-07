e0ef8df9607b0cc11f3b658d88278af95b73441d
Hairun Huang <hairunhuang@gmail.com>
2022-09-06 21:15:57 -0700

    player constructor

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const c = canvas.getContext('2d');
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 200
+        }
+
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        this.image = ''
+        this.width = 100
+        this.height = 100
+    }
+
+
+    
+}
