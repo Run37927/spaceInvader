5b1d139a655feadd05dfbe29471c96d0ce057007
Hairun Huang <hairunhuang@gmail.com>
2022-09-09 07:44:36 -0700

    invader's projectile

--- index.js
+++ index.js
@@ -83,7 +83,29 @@ class Projectile {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
     }
-}
+};
+
+class InvaderProjectile {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+
+        this.width = 3
+        this.height = 10
+
+    }
+
+    draw() {
+        c.fillStyle = 'white'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+};
 
 class Invader {
     constructor({position}) {
