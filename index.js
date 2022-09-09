a3345584f5278623374a1c67b89b5d9d2f4bac20
Hairun Huang <hairunhuang@gmail.com>
2022-09-09 06:17:59 -0700

    create invader class

--- index.js
+++ index.js
@@ -85,6 +85,63 @@ class Projectile {
     }
 }
 
+class Invader {
+    constructor() {
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        this.rotation = 0
+        
+        const image = new Image()
+        image.src = './img/invader.png'
+        image.onload = () => {
+            const scale = 1
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+
+            this.position = {
+                x: canvas.width / 2 - this.width/2,
+                y: canvas.height /2
+            }
+        }
+
+    }
+
+    draw() {
+        // c.fillStyle = 'red'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.save()
+        c.translate(
+            player.position.x + player.width/2, 
+            player.position.y + player.height/2
+            )
+        c.rotate(this.rotation)
+        c.translate(
+            -player.position.x - player.width/2, 
+            -player.position.y - player.height/2
+            )
+
+        c.drawImage(
+            this.image,
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height
+        )
+        c.restore()
+        
+    };
+
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+        }
+    }
+};
 const player = new Player()
 const projectfiles = []
 const keys = {
