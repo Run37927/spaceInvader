96d61e27bfc8d70f3f6da90866125d0d7f0b06f2
Hairun Huang <hairunhuang@gmail.com>
2022-09-17 18:08:44 -0700

    fix canvas size

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log(scoreElement)
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1480
+canvas.height = 750
 
 class Player {
     constructor() {
